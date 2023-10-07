196818eacbf0a32f52247d82c501d2c632e185f3
aldinalj <aldinalj@gmail.com>
2023-10-07 11:15:22 +0200

    Added reroll functionality

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,76 @@
 public class Game {
+    public static final String colourReset = "\u001B[0m";
+    public static final String blueText = "\u001B[34m";
+    public static final String greenText = "\u001B[32m";
+
+    InputHandler ih;
 
     int totalDice;
     int rounds;
-    String[] play;
+    Dice dice;
 
-    public Game(int totalDice) {
+    public Game(int totalDice, InputHandler ih) {
         this.totalDice = totalDice;
         this.rounds = 3;
+        this.dice = new Dice();
+        this.ih = ih;
+    }
+
+    public Player[] play(Player[] players) {
+
+        for (int i = 0; i < rounds; i++) {
+            System.out.println(blueText + "Round " + (i + 1) + ":" + colourReset);
+            ih.takeString();
+
+            for (int j = 0; j < players.length; j++) {
+                System.out.print(greenText + players[j].getName() + colourReset);
+                int roundScore = 0;
+
+                for (int k = 0; k < totalDice; k++) {
+                    ih.takeString();
+                    int diceValue = dice.roll();
+                    System.out.print("Dice " + (k + 1) + ": " + diceValue);
+                    roundScore += diceValue;
+                    ih.clear();
+                }
+                System.out.println();
+                players[j].increaseScore(roundScore);
+            }
+
+        }
+
+        int maxScore = players[0].getScore();
+        int amountOfWinners = 1;
+
+        // Calculates the maximum score and how many has it
+        for (int i = 1; i < players.length; i++) {
+            if (players[i].getScore() > maxScore) {
+                maxScore = players[i].getScore();
+                amountOfWinners = 1;
+            } else if (players[i].getScore() == maxScore) {
+                amountOfWinners++;
+            }
+        }
+
+        // Creates a new array to contain the winners
+        Player[] newPlayers = new Player[amountOfWinners];
+        int newPlayerPointer = 0;
+
+        // Pushes winners into new array
+        for (int i = 0; i < players.length; i++) {
+            if (players[i].getScore() == maxScore) {
+                // Push winner into new array
+                newPlayers[newPlayerPointer] = players[i];
+                newPlayerPointer++;
+            }
+        }
+        if (amountOfWinners > 1) {
+            System.out.println("Seems like the battle of the greatest isn't over...");
+        }
+
+        // Returns new array of players containing only the winners
+        return newPlayers;
+
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,31 @@
 public class Main {
     public static final String colourReset = "\u001B[0m";
     public static final String redText = "\u001B[31m";
-    public static final String blueText = "\u001B[34m";
-    public static final String greenText = "\u001B[32m";
-
     public static void main(String[] args) {
+
         System.out.println("Hello and welcome to the game of the year, the very original \"The Dice Game\". This game requires at least " + redText + "2 players." + colourReset);
 
         InputHandler ih = new InputHandler();
 
-        System.out.println("How many players are you?");
+        int totalPlayers = getTotalPlayers(ih);
 
-        int totalPlayers;
+        Player[] players = getPlayerNames(totalPlayers, ih);
 
-        while (true) {
-            if (ih.hasNumber()) {
-                totalPlayers = ih.takeNumber();
+        int totalDice = getTotalDice(ih);
 
-                if (totalPlayers >= 2) {
-                    break;
-                } else {
-                    System.out.println("You cannot be this total amount of players. Please try again.");
-                }
-            } else {
-                System.out.println("You did not enter a valid number. Please try again.");
-                // Clear buffer
-                ih.clear();
+        Game game = new Game(totalDice, ih);
 
-            }
+       do {
+           players = game.play(players);
+       } while (players.length > 1);
 
-        }
+        Player winner = players[0];
 
-        Player[] players = new Player[totalPlayers];
-        for (int i = 0; i < totalPlayers; i++) {
-            players[i] = new Player();
-        }
+       System.out.println("\n" + winner.getName() + " won with a score of " + winner.getScore() + ". Congratulations!");
 
-        System.out.println("Please enter your desired names!");
-        ih.clear();
+    }
 
-        for (int i = 0; i < totalPlayers; i++) {
-            System.out.print("Player # " + (i + 1) + ": ");
-            players[i].setName(ih.takeString());
-        }
+    private static int getTotalDice(InputHandler ih) {
         System.out.println("How many dice would you like to play with? You can choose between 1-10.");
 
         int totalDice;
@@ -62,46 +45,45 @@ public class Main {
                 ih.clear();
             }
         }
+        return totalDice;
+    }
 
-        Dice dice = new Dice();
-        int rounds = 3;
+    private static Player[] getPlayerNames(int totalPlayers, InputHandler ih) {
+        Player[] players = new Player[totalPlayers];
+        for (int i = 0; i < totalPlayers; i++) {
+            players[i] = new Player();
+        }
 
-        for (int i = 0; i < rounds; i++) {
-            System.out.println( blueText + "Round " + (i+1) + ":" + colourReset);
-            ih.takeString();
+        System.out.println("Please enter your desired names!");
+        ih.clear();
 
-            for (int j = 0; j < totalPlayers; j++) {
-                System.out.print(greenText + players[j].getName() + colourReset);
-                int roundScore = 0;
+        for (int i = 0; i < totalPlayers; i++) {
+            System.out.print("Player # " + (i + 1) + ": ");
+            players[i].setName(ih.takeString());
+        }
+        return players;
+    }
 
-                for (int k = 0; k < totalDice; k++) {
-                    ih.takeString();
-                    int diceValue = dice.roll();
-                    System.out.print("Dice " + (k + 1) + ": " + diceValue);
-                    roundScore += diceValue;
-                    ih.clear();
+    private static int getTotalPlayers(InputHandler ih) {
+        System.out.println("How many players are you?");
+        int totalPlayers;
+        while (true) {
+            if (ih.hasNumber()) {
+                totalPlayers = ih.takeNumber();
+
+                if (totalPlayers >= 2) {
+                    break;
+                } else {
+                    System.out.println("You cannot be this total amount of players. Please try again.");
                 }
-                System.out.println();
-                players[j].increaseScore(roundScore);
+            } else {
+                System.out.println("You did not enter a valid number. Please try again.");
+                // Clear buffer
+                ih.clear();
+
             }
 
         }
-
-        int maxScore = players[0].getScore();
-        int winnerIndex = 0;
-        // int[] winnersLeft = players[0].getScore();
-
-        for (int i = 1; i < totalPlayers; i++) {
-            if (players[i].getScore() > maxScore) {
-                maxScore = players[i].getScore();
-                winnerIndex = i;
-            }
-           /* if (players[i].getScore() == maxScore) {
-                System.out.println("Seems like the battle of the greatest isn't over...");
-            } */
-        }
-
-        String winner = players[winnerIndex].getName();
-        System.out.println("\n" + winner + " won with a score of " + maxScore + ". Congratulations!");
+        return totalPlayers;
     }
 }
